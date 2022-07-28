089a6981f241a2c6f6962627834bcefcef29422e
Nick <nickhit3802@gmail.com>
2022-07-28 13:32:21 -0700

    Create-checklist button now creates new checklist with tasks

--- web/src/pages/CreateTemplatePage/CreateTemplatePage.js
+++ web/src/pages/CreateTemplatePage/CreateTemplatePage.js
@@ -1,6 +1,6 @@
 import { Form, TextField, Submit, useForm } from '@redwoodjs/forms'
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags, useMutation } from '@redwoodjs/web'
+import { Link, routes, navigate } from '@redwoodjs/router'
+import { MetaTags, useMutation, useQuery } from '@redwoodjs/web'
 import TaskListCell, { QUERY } from 'src/components/TaskListCell'
 import TemplateListHeaderCell from 'src/components/TemplateListHeaderCell'
 
@@ -12,26 +12,86 @@ const CREATE_TASK = gql`
   }
 `
 
+const CREATE_CHECKLIST = gql`
+  mutation CreateChecklistMutation($input: CreateChecklistInput!) {
+    createChecklist(input: $input) {
+      id
+    }
+  }
+`
+
+const GET_TEMPLATE = gql`
+query FindTemplateQuery($id: Int!) {
+  template: checklist(id: $id) {
+    title
+    description
+  }
+}
+`
+
 const CreateTemplatePage = ({ id }) => {
   const formMethods = useForm()
 
+  // Replace error with data
+  const { error } = useQuery(GET_TEMPLATE)
   const [create, { loading }] = useMutation(CREATE_TASK, {
     onCompleted: () => {
       formMethods.reset()
     }
   })
-  const onSubmit = (data) => {
-    console.log(data)
+
+  // Essentially duplicate checklist
+  // Need to get this template's title and tasks
+  // After mutation function executed, user is taken to home
+  const [createChecklist, { data }] = useMutation(CREATE_CHECKLIST, {
+    onCompleted: () => {
+      // Test data
+      const taskList = [
+        { body: "temp1", completed: false, checklistId: 2 },
+        { body: "temp2", completed: false, checklistId: 2 },
+        { body: "temp3", completed: false, checklistId: 2 }
+      ]
+
+      // Replace with the template's tasks
+      taskList.forEach(() => (
+        create({
+          variables: {
+            input: {
+              body: "New",
+              description: "New",
+              completed: false,
+              checklistId: data.createChecklist.id
+            }
+          }
+        })
+      ))
+      navigate(routes.home()) // Potentially change location?
+    }
+  })
+
+  const onSubmit = (formData) => {
     create({
       variables: {
         input: {
-          body: data.body,
-          description: data.description,
+          body: formData.body,
+          description: formData.description,
           completed: false,
           checklistId: id
         }
       },
-      refetchQueries: [{ query: QUERY }, 'FindTaskListQuery' ]
+      refetchQueries: [{ query: QUERY }, 'FindTaskListQuery']
+    })
+  }
+
+  // Replace input with this template's data
+  const onClick = () => {
+    createChecklist({
+      variables: {
+        input: {
+          title: "Temp",
+          description: "Temp"
+        }
+      }
     })
   }
 
@@ -42,7 +102,7 @@ const CreateTemplatePage = ({ id }) => {
       <h1>This is my checklist ID: {id}</h1>
 
       <div className="template">
-        <TemplateListHeaderCell id={id}/>
+        <TemplateListHeaderCell id={id} />
 
         <div className="template-body">
           <TaskListCell id={id} />
@@ -54,6 +114,10 @@ const CreateTemplatePage = ({ id }) => {
           </Form>
         </div>
       </div>
+
+      <Form className="new-checklist-form" onSubmit={onClick} >
+        <Submit>Testing create checklist</Submit>
+      </Form>
     </>
   )
 }
